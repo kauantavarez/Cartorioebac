c76626a29dd7561b6b0834079929c33c0a99c00c
Kauan Tavares Dev <163591443+kauantavarez@users.noreply.github.com>
2024-03-15 17:33:16 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- tikkauan.c
+++ tikkauan.c
@@ -128,6 +128,7 @@ int main()
      printf("\t1 - Registrar nomes\n");
      printf("\t2 - Consultar nomes\n");
      printf("\t3 - Deletar nomes\n");
+     printf("\t4 - Sair do sistema\n\n");
      printf("Opção: "); //fim do menu
       
      scanf("%d" , &opcao); //armazenando escolha do usuário   
@@ -146,6 +147,11 @@ int main()
 		
 		  case 3:
 		  deletar();
+		  break;
+		  
+		  case 4:
+		  printf("Obrigado por utilizar esse sistema!\n");
+		  return 0;
 		  break;
 		
 	  	  default:
